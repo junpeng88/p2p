bb70ca8bd42b4fe235fe74b98bcd9e150ff4dbe6
王俊朋 <1832907879@qq.com>
2016-12-25 14:47:01 +0800

    2222222222222

---
+++ src/com/money/newFile.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.money;
+
+/** 
+ * @包名 com.money
+ * @类名 newFile.java
+ * @作者  wangjunpeng
+ * @创建日期 2016年12月25日
+ * @描述 
+ * @版本 V 1.0
+ */
+
+public class newFile {
+    
+    public static void main(String[] args) {
+		System.out.println("today is good");
+	}
+}
