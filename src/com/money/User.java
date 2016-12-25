179a40f234ffa09a73c38467d36855636985fcd8
王俊朋 <1832907879@qq.com>
2016-12-25 13:21:07 +0800

    commit p2p java file

---
+++ src/com/money/User.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.money;
+
+/** 
+ * @包名 com.money
+ * @类名 User.java
+ * @作者  wangjunpeng
+ * @创建日期 2016年12月25日
+ * @描述 
+ * @版本 V 1.0
+ */
+
+public class User {
+  
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I want to become a perfect IT man");
+	} 
+}
